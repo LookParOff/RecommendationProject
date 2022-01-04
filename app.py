79a6177fd08a647aab1c13b7d5bf8f9e1f75bd03
Nikita <20956901+LookParOff@users.noreply.github.com>
2022-01-04 18:35:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+
+if os.environ.get("ON_HEROKU"):
+	port = os.environ.get("port")
+	app.run(host="0.0.0.0", port=port, debug=True)
+else:
+	app.run(host="127.0.0.1", port=5000, debug=True)
