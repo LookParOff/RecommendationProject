c78f9adac6ab8bddb79e85d5ab0cc436188db28e
Nikita <20956901+LookParOff@users.noreply.github.com>
2022-01-05 14:09:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.route("/recs", methods=['POST'])
 def recs():
-	# TODO input: json of ratings of man. output: json of recommended books, like jsons for Andrew
-	print("I in recs. Ready to catch your request")
-	input_json = request.get_json()  # force=True
-	print("I catch. And I store request, here it is:")
-	print(input_json)
-	print("END!")
-	return input_json
+    # TODO input: json of ratings of man. output: json of recommended books, like jsons for Andrew
+    print("I in recs. Ready to catch your request")
+    input_json = request.get_json()  # force=True
+    print("I catch. And I store request, here it is:")
+    print(input_json)
+    print("END!")
+    return input_json
