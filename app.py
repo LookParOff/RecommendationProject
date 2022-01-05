91038030a38343503a1ebc2ebbf1d09348cd9720
Nikita <20956901+LookParOff@users.noreply.github.com>
2022-01-05 14:26:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 from flask import Flask, render_template, request
+"""
+Backend of sending recommendations
+"""
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    """
+    just showing black 
+    :return:
+    """
     return render_template("index.html")
 
 
 @app.route("/recs", methods=['POST'])
 def recs():
-    # TODO input: json of ratings of man. output: json of recommended books, like jsons for Andrew
+    """
+    function handle answering on post request.
+    input: json of ratings of man. output: json of recommended books, like jsons for Andrew
+    :return:
+    """
     print("I in recs. Ready to catch your request")
     input_json = request.get_json()  # force=True
     print("I catch. And I store request, here it is:")
